b2669fd029a30fabcbeb72a35ccf7a21655e4317
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2026-06-08 20:05:30 +0300

    Figure out mediator

---
+++ app/application/api/main.py
@@ -0,0 +1,44 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from aiojobs import Scheduler
+from punq import Container
+
+from application.api.lifespan import (
+    close_message_broker,
+    consume_in_background,
+    init_message_broker,
+)
+from application.api.messages.handlers import router as message_router
+from application.api.messages.websockets.messages import router as message_ws_router
+from logic.init import init_container
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_message_broker()
+
+    container: Container = init_container()
+    scheduler: Scheduler = container.resolve(Scheduler)
+
+    job = await scheduler.spawn(consume_in_background())
+
+    yield
+    await close_message_broker()
+    # TODO: закрыть консьюмеры
+    await job.close()
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Simple Kafka Chat",
+        docs_url="/api/docs",
+        description="A simple kafka + ddd example.",
+        debug=True,
+        lifespan=lifespan,
+    )
+    app.include_router(message_router, prefix="/chats")
+    app.include_router(message_ws_router, prefix="/chats")
+
+    return app
